61a994d033646be3eb1f4182ccaaaef0cc10d462
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-01-12 13:54:52 +0100

    Retrieve random posts

---
+++ services/postService/retrievePosts.js
@@ -0,0 +1,30 @@
+const retrievePosts = (type, medior, username) => {
+    switch(type) {
+        case 'GSR':
+            return switchGSRValue(medior, username)
+        break;
+
+        case 'Pulse':
+            return switchPulseValue(medior, username)
+        break;
+    }
+}
+
+const switchGSRValue = (medior, username) => {
+    switch(medior) {
+        case '':
+        break;
+        case '':
+        break;
+    }
+}
+
+const switchPulseValue = (medior, username) => {
+    switch(medior) {
+        case '':
+        break;
+        case '':
+        break;
+    }
+}
+    
