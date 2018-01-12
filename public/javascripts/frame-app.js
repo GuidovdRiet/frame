a85e7e471526a4dd63ea22529e81cdad82045e60
Guido van de Riet <guidovdriet@gmail.com>
2018-01-12 13:35:38 +0100

    Add followers functionality and display user information on profile

--- public/javascripts/frame-app.js
+++ public/javascripts/frame-app.js
@@ -1,3 +1,20 @@
 // Import scss for webpack compiling
 require('../sass/app.scss');
 require('../../services/sensorDataService/renderGraphs');
+
+const axios = require('axios');
+
+window.addEventListener('load', () => {
+    const followForm = document.querySelector('.user-card__follow-form');
+    followForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        axios
+            .post(e.target.action)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+});
